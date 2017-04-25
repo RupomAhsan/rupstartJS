20e7d51613db1f90832d0c24c63b6762da613d37
Ahsan Al Rupom <ahsanal.rupom@scicom.com.my>
2017-04-25 14:44:35 +0800

    Added babel and update ES6 Scripts

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,8 @@
-var express =require('express');
+import express from 'express';
 
-var path =require('path');
+import path from 'path';
 
-var open =require('open');
+import open from 'open';
 
 //var webpack =require('webpack');
 
@@ -10,9 +10,9 @@ var open =require('open');
 
 
 
-var port = 3000;
+const port = 3000;
 
-var app = express();
+const app = express();
 
 //var compiler = webpack(config);
 
