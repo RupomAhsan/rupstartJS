853977b79d19cb920514e17b9739f14a96a64e05
Rupom Ahsan <rupomahsan@live.com>
2017-04-28 02:46:07 +0800

    Separate Bundle

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,46 +2,51 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-export default{
-    debug:true,
-    devtool:'source-map',
-    noInfo:false,
-    entry:[
-        path.resolve(__dirname,'src/index')
-    ],
-    target:'web',
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        publicPath:'/',
-        filename:'bundle.js'
-    },
-    plugins:[
-      //Create HTML file that includes references to bundled JS
-      new HtmlWebpackPlugin({
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  plugins: [
+    //To create separate bundle, so that they can cached separately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    //Create HTML file that includes references to bundled JS
+    new HtmlWebpackPlugin({
       template: 'src/index.html',
-      minify:{
-        removeComments:true,
-        collapseWhitespace:true,
-        removeRedundantAttributes:true,
-        useShortDoctype:true,
-        removeEmptyAttributes:true,
-        removeStyleLinkTypeAttributes:true,
-        keepClosingSlash:true,
-        minifyJS:true,
-        minifyCSS:true,
-        minifyURLs:true
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
       },
       inject: true
     }),
-      //Eliminate Duplicate Packages when bundle creating
-      new webpack.optimize.DedupePlugin(),
-      //Minify JS
-      new webpack.optimize.UglifyJsPlugin()
-    ],
-    module:{
-        loaders:[
-            {test:/\.js$/,exclude:/node_modules/,loaders:['babel']},
-            {test:/\.css$/,loaders:['style-loader','css-loader']}
-        ]
-    }
+    //Eliminate Duplicate Packages when bundle creating
+    new webpack.optimize.DedupePlugin(),
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+    ]
+  }
 }
