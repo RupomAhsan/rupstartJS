1ca69568c1da63364936bf289ba17fb6bca34d62
Rupom Ahsan <rupomahsan@live.com>
2017-04-28 00:00:19 +0800

    Add users API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,7 +21,13 @@ app.get('/', function(req, res) {
 
 });
 
-
+app.get('/users', function(req, res) {
+     res.json([
+      {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+      {"id":2,"firstName":"Bob2","lastName":"Smith2","email":"bob2@gmail.com"},
+      {"id":3,"firstName":"Bob3","lastName":"Smith3","email":"bob3@gmail.com"}
+    ]);
+});
 
 app.listen(port, function(err) {
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import './index.css';
+import {getUsers} from './api/userApi';
 
-import numeral from 'numeral';
-
-const courseValue=numeral(1000).format('$0,0.00');
-//debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+//Populate the table of users using user API
+let usersContent = '';
+  getUsers().then((users) => {
+    users.forEach((user) => {
+      usersContent += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}
+      </tr>`;
+    });
+    global.document.getElementById('users').innerHTML = usersContent;
+});
