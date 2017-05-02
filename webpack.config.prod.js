6d7bb05fd214a7173d6a634ab2b6d707cd1f2378
Ahsan Al Rupom <ahsanal.rupom@scicom.com.my>
2017-05-02 12:24:08 +0800

    Fixed webpack config for production (without css minify)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,9 +5,9 @@ import WebpackMd5Hash from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  debug: true,
+  //debug: true,
   devtool: 'source-map',
-  noInfo: false,
+  //noInfo: false,
   entry: {
     vendor: path.resolve(__dirname, 'src/vendor'),
     main: path.resolve(__dirname, 'src/index')
@@ -17,11 +17,15 @@ export default {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: '[name].[chunkhash].js'
+    //,
+    //sourceMapFilename: '[name].[chunkhash].map'
   },
   plugins: [
 
     //Generate an external Css file
-    new ExtractTextPlugin('[name].[contenthash].css'),
+    new ExtractTextPlugin({
+    filename:'[name].[contenthash].css'}),
+   // new ExtractTextPlugin("styles.css"),
     //Hash the filename so that the name changes when content changes
     new WebpackMd5Hash(),
     //To create separate bundle, so that they can cached separately
@@ -40,7 +44,7 @@ export default {
         removeStyleLinkTypeAttributes: true,
         keepClosingSlash: true,
         minifyJS: true,
-        minifyCSS: true,
+        minifyCSS: false,
         minifyURLs: true
       },
       inject: true
@@ -48,14 +52,42 @@ export default {
     //Eliminate Duplicate Packages when bundle creating
     new webpack.optimize.DedupePlugin(),
     //Minify JS
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    })
   ],
-  module: {
+  /*module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
       { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
      // { test: /\.css$/, loaders: ['style', 'css'] }
      // { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', '!css-loader?sourceMap&importLoaders=1!postcss-loader')}
+    ]
+  }*/
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader'
+        }]
+      }
+      ,
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader?sourceMap"
+        })
+      }
     ]
   }
 }
